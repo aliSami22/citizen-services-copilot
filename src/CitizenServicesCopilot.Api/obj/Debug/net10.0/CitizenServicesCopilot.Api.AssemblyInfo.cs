2d317abd5d94291d631e17c078dd606acf2eb0fc
aliSami22 <alisamielsayed22@gmail.com>
2026-09-15 18:51:03 +0300

    chore: generate debug build artifacts and compiled binaries

--- src/CitizenServicesCopilot.Api/obj/Debug/net10.0/CitizenServicesCopilot.Api.AssemblyInfo.cs
+++ src/CitizenServicesCopilot.Api/obj/Debug/net10.0/CitizenServicesCopilot.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitizenServicesCopilot.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59942e9d91e6081c43268191766a17384d9e007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b38168523531039cf5948cf8943a9d86ab5746e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitizenServicesCopilot.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitizenServicesCopilot.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CitizenServicesCopilot.Infrastructure/obj/Debug/net10.0/CitizenServicesCopilot.Infrastructure.AssemblyInfo.cs
+++ src/CitizenServicesCopilot.Infrastructure/obj/Debug/net10.0/CitizenServicesCopilot.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitizenServicesCopilot.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59942e9d91e6081c43268191766a17384d9e007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b38168523531039cf5948cf8943a9d86ab5746e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitizenServicesCopilot.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitizenServicesCopilot.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
